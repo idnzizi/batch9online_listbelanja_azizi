3f3eaca2c4d475fd7bcf5f88e2522c92b7d7fa5a
idnzizi <109082652+idnzizi@users.noreply.github.com>
2022-09-04 16:23:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-gcvqdz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gcvqdz)
---
+++ script.js
@@ -0,0 +1,101 @@
+// tangkap beberapa element
+
+let modal = document.getElementById('modal');
+let floating_button = document.getElementById('floating_button');
+let modal_bg = document.getElementById('modal_bg');
+let addlist_form = document.getElementById('addlist_form');
+let root = document.getElementById('root');
+let subtitle = document.getElementById('subtitle');
+
+//tambahkan date ke subtitle
+subtitle.innerHTML = new Date().toLocaleDateString();
+
+//data list belanja
+let data_list_belanja = [];
+
+//menambahkan event listener ke floating button
+floating_button?.addEventListener('click', () => {
+  // munculkan modal
+
+  if (modal.style.display == 'none') {
+    showModal();
+    return;
+  }
+
+  // sembunyikan kembali
+  hideModal();
+});
+
+//menambahkan event listener ke modal bg
+modal_bg?.addEventListener('click', () => {
+  // sembunyikan kembali
+  hideModal();
+});
+
+//tambahkan event listener submit ke addlist form
+addlist_form?.addEventListener('submit', (event) => {
+  //stop form dari reload page
+  event.preventDefault();
+
+  //tangkap value dari masing masing input field
+  let barang = event.target.barang.value;
+  let harga = event.target.harga.value;
+
+  //push data ke data list belanja
+  data_list_belanja.push({
+    nama_barang: barang,
+    harga_barang: harga,
+    tanggal: new Date().toLocaleDateString(),
+  });
+
+  console.info(data_list_belanja);
+
+  //clear input field
+  (event.target.barang.value = ''),
+    (event.target.harga.value = ''),
+    hideModal();
+  renderToHtml();
+});
+
+//show modal
+function showModal() {
+  modal.style.display = 'flex';
+  floating_button.style.display = '#C0C0C0';
+  floating_button.style.transform = 'rotate(45deg)';
+}
+
+//hide modal
+function hideModal() {
+  modal.style.display = 'none';
+  floating_button.style.display = '#39ff14';
+  floating_button.style.transform = 'rotate(0deg)';
+}
+
+//function render to html
+function renderToHtml() {
+  //clear element div
+  root.innerHTML = '';
+
+  //perulangan
+  data_list_belanja.forEach((e, i) => {
+    root.innerHTML += `
+    <div class="card">
+      <small> ${e.tanggal} </small>
+      <div>
+      ${e.nama_barang} <span> ${e.harga_barang} </span>
+      </div>
+      <button onclick="handleDelete(${i})">Selesai</button>
+
+    </div>
+
+    `;
+  });
+}
+
+//function delete items array datalistbelanja
+function handleDelete(index) {
+  data_list_belanja.splice(index, 1);
+
+  renderToHtml();
+}
+
